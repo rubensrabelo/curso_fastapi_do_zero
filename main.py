ad43c68e87d5d9a1650d6ba9c9c53f9ea4712390
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-02 17:45:42 -0300

    Adding status_code in the read_root method

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from starlette import status
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 def read_root():
     return {
         "Message": "Hello, world!"
