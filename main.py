e6188d2a872c83a1c778c44a93e2f99344d4e767
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-07 10:52:31 -0300

    Adding user schemas, post endpoint and database with list

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from starlette import status
 
-from schemas import Message
+from schemas import Message, UserPublic, UserSchema, UserDB
 
 app = FastAPI()
+
+database = []
 
 
 @app.get(
@@ -15,3 +17,14 @@ def read_root():
     return {
         "message": "Hello, world!"
     }
+
+
+@app.post(
+        "/",
+        status_code=status.HTTP_201_CREATED,
+        response_model=UserPublic
+        )
+def create_user(user: UserSchema):
+    user_db = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_db)
+    return user_db
--- schemas.py
+++ schemas.py
@@ -1,5 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Message(BaseModel):
     message: str
+
+
+class UserPublic(BaseModel):
+    username: str
+    email: EmailStr
+
+
+class UserSchema(UserPublic):
+    password: str
+
+
+class UserDB(UserSchema):
+    id: int
