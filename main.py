95f7cf617cb9361bfb2e9bbb604df72bad8bd1e4
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-11 06:48:40 -0300

    Implementing the DELETE Route

--- main.py
+++ main.py
@@ -57,3 +57,19 @@ def update_user(user_id: int, user: UserSchema):
     user_with_id.id = user_id
     database[user_id - 1] = user_with_id
     return user_with_id
+
+
+@app.delete(
+        "/users/{user_id}",
+        response_model=Message
+)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+    del database[user_id - 1]
+    return {
+        "message": "User deleted"
+    }
