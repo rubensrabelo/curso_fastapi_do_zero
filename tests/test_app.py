12fa6b007ab4e2dcc5b8f3a332986720c65413f1
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-11 18:48:13 -0300

    Implementing the GET USER BY ID and 404 (NOT FOUND) error tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -41,6 +41,24 @@ def test_read_users(client):
     }
 
 
+def test_read_user_by_id(client):
+    response = client.get("/users/1")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+                "username": "alice",
+                "email": "alice@example.com",
+                "id": 1
+            }
+
+
+def test_404_for_read_user_by_id(client):
+    response = client.get("/users/0")
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
+
+
 def test_update_user(client):
     response = client.put(
         "/users/1",
