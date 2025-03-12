a5f5a15172da79721af91ea47fb30e9e5767faf2
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-12 14:36:46 -0300

    Creating a Fixture for Database Interactions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 import sys
 import os
 
@@ -9,8 +11,20 @@ sys.path.insert(
     )
 
 from main import app  # noqa: E402
+from models import table_registry  # noqa: E402
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine("sqlite:///:memory:")
+    table_registry.metada.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
