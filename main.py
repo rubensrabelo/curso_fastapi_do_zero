e955f5818346d8775eb5705720f01b41dc9fdf28
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-16 12:28:59 -0300

    Modifying the POST /users Endpoint

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 from starlette import status
 
+from database import get_session
+from models import User
 from schemas import Message, UserPublic, UserSchema, UserList
 
 app = FastAPI()
-
-database = []
 
 
 @app.get(
@@ -24,11 +26,33 @@ def read_root():
         status_code=status.HTTP_201_CREATED,
         response_model=UserPublic
         )
-def create_user(user: UserSchema):
-    user_db = UserPublic(**user.model_dump())
-    user_db.id = len(database) + 1
-    database.append(user_db)
-    return user_db
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
+
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Username already exists"
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Email already exists"
+            )
+
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get(
