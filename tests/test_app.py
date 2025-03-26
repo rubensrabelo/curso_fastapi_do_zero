e43643dcfaf7b484f2d22388c6ad4e169dfa3483
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-26 13:58:33 -0300

    Test authentication if the email is sent, but there is no User registered in the database

--- tests/test_app.py
+++ tests/test_app.py
@@ -161,3 +161,16 @@ def test_get_current_user_not_found(client):
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_get_current_user_does_not_exists__exercicio(client):
+    data = {'sub': 'test@test'}
+    token = create_access_token(data)
+
+    response = client.delete(
+        '/users/1',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
