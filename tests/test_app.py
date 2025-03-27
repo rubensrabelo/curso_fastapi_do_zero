4ad5894cacde169c6454dff12873e6034c1cfc10
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-27 12:36:45 -0300

    Fixing the tests with the token

--- tests/test_app.py
+++ tests/test_app.py
@@ -59,7 +59,7 @@ def test_create_user_409_email_already_exists(client, user):
 
 def test_get_token(client, user):
     response = client.post(
-        "/token",
+        "/auth/token",
         data={"username": user.email, "password": user.clean_password}
     )
     token = response.json()
