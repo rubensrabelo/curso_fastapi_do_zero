f612573077af401a46cd3f518623cbd804702e2a
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-28 10:11:36 -0300

    Moving constants to environment variables

--- security.py
+++ security.py
@@ -10,10 +10,9 @@ from starlette import status
 
 from database import get_session
 from models import User
+from settings import Settings
 
-SECRET_KEY = "your-secret-key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+settings = Settings()
 pwd_context = PasswordHash.recommended()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
@@ -21,10 +20,12 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.now(tz=ZoneInfo("UTC")) + timedelta(
-        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
     to_encode.update({"exp": expire})
-    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = encode(to_encode,
+                         settings.SECRET_KEY,
+                         algorithm=settings.ALGORITHM)
     return encoded_jwt
 
 
@@ -47,7 +48,9 @@ def get_current_user(
     )
 
     try:
-        payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = decode(token,
+                         settings.SECRET_KEY,
+                         algorithms=[settings.ALGORITHM])
         subject_email = payload.get("sub")
 
         if not subject_email:
